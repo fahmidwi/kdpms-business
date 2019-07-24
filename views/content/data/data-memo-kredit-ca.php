6a8fd7662b8e568379dc7ee965828af2017250f8
Fahmi dwi <fahmidwi45@gmail.com>
2019-07-24 13:33:07 +0700

    rubah proses add data memo ca

--- views/content/data/data-memo-kredit-ca.php
+++ views/content/data/data-memo-kredit-ca.php
@@ -115,23 +115,24 @@
 				paramsID2.val(id_calon_debitur)
 				paramsID3.val(id_kredit_checking)
 				$('#modal').modal('toggle')
-				// if (jaminan == 'SERTIFIKAT') {
-				// 	 setTimeout(function () {
-				// 	 	load()
-				// 	 	$('#tamplate').val('views/content/form/add-memo-kredit-ca.php')
-				// 	 	$('#app').load('views/content/form/add-memo-kredit-ca.php')
-				// 	 },500)
-				// 	//checkOrder(id_order,jaminan)
-				// }else if (jaminan == 'BPKB'){
-				// 	//	alert('ANGUNAN KENDARAAN# FITUR SEDANG DALAM PROSES PERBAIKAN')
-				// 	load()
-				// 	 setTimeout(function () {
-				// 	 	load()
-				// 	 	$('#tamplate').val('views/content/form/add-memo-kredit-ca-kendaraan.php')
-				// 	 	$('#app').load('views/content/form/add-memo-kredit-ca-kendaraan.php')
-				// 	 },500)
-					checkOrder(id_order,jaminan)
-				}
+				// // if (jaminan == 'SERTIFIKAT') {
+				// // 	 setTimeout(function () {
+				// // 	 	load()
+				// // 	 	$('#tamplate').val('views/content/form/add-memo-kredit-ca.php')
+				// // 	 	$('#app').load('views/content/form/add-memo-kredit-ca.php')
+				// // 	 },500)
+				// // 	//checkOrder(id_order,jaminan)
+				// // }else if (jaminan == 'BPKB'){
+				// // 	//	alert('ANGUNAN KENDARAAN# FITUR SEDANG DALAM PROSES PERBAIKAN')
+				// // 	load()
+				// // 	 setTimeout(function () {
+				// // 	 	load()
+				// // 	 	$('#tamplate').val('views/content/form/add-memo-kredit-ca-kendaraan.php')
+				// // 	 	$('#app').load('views/content/form/add-memo-kredit-ca-kendaraan.php')
+				// // 	 },500)
+				// 	checkOrder(id_order,jaminan)
+				// }
+				checkOrder (id_order,jaminan)
 		})
 		function checkOrder (id_order,jaminan) {
 			$.ajax({
